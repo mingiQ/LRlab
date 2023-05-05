87bc44fb12582956815e9bccccc78caae378ac00
MINGI <37833625+jevais@users.noreply.github.com>
2023-05-05 16:35:08 -0400

    Update MicroLambda_BandpassFilter.py
    debug serial communication

--- Experiment/Instruments/MicroLambda_BandpassFilter.py
+++ Experiment/Instruments/MicroLambda_BandpassFilter.py
@@ -6,28 +6,25 @@ import json
 import time
 import sys
 
-rm = pyvisa.ResourceManager()
-def numtostr(mystr):`
-    return '%20.15e' % mystr
 
-## MicroLambda bandpass filter --> need to fix!
+## MicroLambda bandpass filter --> need to fix!  (fixed! 2023/5/5)
 class MLBPF:
     def __init__(self, addr):
-        self.arduino = serial.Serial(port=str(addr), baudrate=115200, timeout=0.1)
+        self.arduino = serial.Serial(port=str(addr), baudrate=115200, timeout=None)
     def write_read(self, x):
         self.arduino.write(bytes(x, 'utf-8'))
         time.sleep(0.05)
-        data = self.arduino.readline().decode('utf-8').rstrip()
+        data = self.arduino.readline().decode().rstrip()
         print(data)
     def idn(self):
         self.arduino.write(bytes('?', 'utf-8'))
         time.sleep(0.05)
-        data = self.arduino.readline().decode('utf-8').rstrip()
+        data = self.arduino.readline().decode().rstrip()
         print(data)
     def freq(self, f):
         self.arduino.write(bytes('FH' + ' ' + str(f), 'utf-8'))
         time.sleep(0.05)
-        data = self.arduino.readline().decode('utf-8').rstrip()
+        data = self.arduino.readline().decode().rstrip()
         print(data)
     def quit_bpf(self):
         self.arduino.close()
