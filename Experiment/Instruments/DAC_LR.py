96147057eacbaf1abcb1467fe836a06f79b5352d
MINGI <37833625+jevais@users.noreply.github.com>
2023-05-05 16:39:48 -0400

    Update DAC_LR.py
    Modify Serial import timeout = None ...
    just as ML BPF code

--- Experiment/Instruments/DAC_LR.py
+++ Experiment/Instruments/DAC_LR.py
@@ -6,20 +6,17 @@ import json
 import time
 import sys
 
-rm = pyvisa.ResourceManager()
-def numtostr(mystr):`
-    return '%20.15e' % mystr
 
 ## DAC LR
 class DAC_CF:
     def __init__(self, addr):       # COM4
-        self.DAC = serial.Serial(port=str(addr), baudrate=115200, timeout=.1)
+        self.DAC = serial.Serial(port=str(addr), baudrate=115200, timeout=None)
     def idn(self):
         self.DAC.write(bytes('*IDN?', 'utf-8'))
-        data = self.DAC.readline().decode('utf-8').rstrip()
+        data = self.DAC.readline().decode().rstrip()
         print(data)
     def voltsweep(self, dac_CF, volt):
         self.DAC.write(bytes("WF "+ str(dac_CF) +" "+ str(volt) +" "+ str(volt), 'utf-8'))
         time.sleep(0.05)
-        data = self.arduino.readline().decode('utf-8').rstrip()
+        data = self.arduino.readline().decode().rstrip()
         print(data)
